9e4f25277a533f669f45adb48419f85041cea5c1
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-04-01 22:54:47 -0400

    created for loop for nav bar to make styling easier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,20 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed bottom-0 md:top-0 left-0 right-0   bg-white w-100 md:h-24 z-0 flex justify-center md:justify-end border-t-4 md:border-t-0 md:border-b-4 border-primary dark:text-white dark:bg-neutral">
-      <ul className="flex gap-10 h-100 p-2 text-xs md:text-xl lg:text-3xl">
-        <li>
-          <a
-            href="#hero"
-            aria-label="Click to go to top"
-            className="hover:text-primary"
-          >
-            Back to Top{" "}
-          </a>
-        </li>
-        <li>
-          <a href="#about" aria-label="Click to go to about me">
-            {" "}
-            About Me
-          </a>
-        </li>
-        <li>
-          <a href="#projects" aria-label="Click to go to projects">
-            Projects{" "}
-          </a>
-        </li>
-        <li>
-          <a href="#form" aria-label="Click to go to contact form">
-            Contact
-          </a>
-        </li>
+    <nav className="fixed bottom-0 md:top-0 left-0 right-0    bg-white w-100 md:h-24 z-0 flex justify-center md:justify-end border-t-4 md:border-t-0 md:border-b-4 border-primary dark:text-white dark:bg-neutral">
+      <ul className="flex gap-10  p-2 text-xs md:text-xl lg:text-3xl">
+        {[
+          ["#hero", "Click to go to about me", "Back to Top"],
+          ["#about", "Click to go to about me", "About Me"],
+          ["#projects", "Click to go to projects", "Projects"],
+          ["#form", "click to go to contact", "Contact"],
+        ].map(([url, aria, title]) => (
+          <li key={url}
+		  className="self-center">
+            <a href={url}
+			className="active:text-primary cursor-pointer ">{title}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
